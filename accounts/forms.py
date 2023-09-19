3335c80dd48771dd262c197016994242e925acc8
abas_jvnbkht <abas.javanbakht90@gmail.com>
2023-09-19 12:36:44 +0330

    restaurant profile + google geocoding

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User, Vendor
+from .models import User, Vendor, UserProfile
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -21,6 +22,34 @@ class UserForm(forms.ModelForm):
 
 
 class VendorForm(forms.ModelForm):
+    vendor_licence = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_licence']
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    # latitude = forms.CharField(
+    #     widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # logitude = forms.CharField(
+    #     widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # in blaei ha ba def __init__ yeki ast
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address',
+                  'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'logitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
